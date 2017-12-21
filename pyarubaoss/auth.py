d6b00b34b271da950be56ccfe108ce66cfabe568
Pack3tL0ss <wade99@gmail.com>
2017-12-21 02:22:53 -0500

    Update auth.py
    Updated auth to use version as parameter vs. hard-coded v1.  v1 returns 404 in 16.04.0008

--- pyarubaoss/auth.py
+++ pyarubaoss/auth.py
@@ -8,8 +8,8 @@ class AOSSAuth():
     This class requests and stores an authentication cookie for the Aruba AOS
     Switch Software.
     """
-    def __init__(self, switchip, username, password, version="v3"):
-        url_login = "http://" + switchip + "/rest/v1/login-sessions"
+    def __init__(self, switchip, username, password, version):
+        url_login = "http://" + switchip + "/rest/" + version + "/login-sessions"
         payload_login = {"userName": username, "password": password}
         s = requests.Session()
         response = s.post(url_login, data=json.dumps(payload_login), timeout=1)
@@ -19,7 +19,7 @@ class AOSSAuth():
 
 
     def logout(self):
-        url_login = "http://" + self.ipaddr + "/rest/v1/login-sessions"
+        url_login = "http://" + self.ipaddr + "/rest/" + version + "/login-sessions"
         r = requests.delete(url_login, headers=self.cookie)
         return r.status_code
 
